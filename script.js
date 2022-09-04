40c735272ee3c6a61e5268e8863ce6e0a3b68c16
Anmol <anmolp476@gmail.com>
2022-09-04 13:40:39 -0400

    Added Firefox compatibility

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getQuote()
 {
-    const response = await fetch("https://api.adviceslip.com/advice")
+    const response = await fetch("https://api.adviceslip.com/advice",  {cache: 'no-cache'})
     const quoteSlip = await response.json();
     const quote = quoteSlip.slip.advice;
     const quoteNum = quoteSlip.slip.id;
